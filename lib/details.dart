d51bd730c6edc1bb98d84e308ccd6d5a340311af
Florian <florian.desmortreux@etudiant.univ-rennes.fr>
2022-11-23 15:53:03 +0100

    details page & shellroute

---
+++ lib/details.dart
@@ -0,0 +1,93 @@
+import 'package:festival/models/event_status.dart';
+import 'package:festival/models/festival.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+Festival f = Festival(
+    "FEST_30003_24",
+    EventStatus.COMPLETE,
+    "Écran libre - festival international du court-métrage",
+    "Aigues-Mortes",
+    "30003",
+    "Cinéma, audiovisuel",
+    "http://ecranlibre-aiguesmortes.fr",
+    "",
+    "ecranlibre.aiguesmortes@orange.fr",
+    30,
+    43.566091,
+    4.189267);
+
+class DetailsPage extends StatefulWidget {
+  const DetailsPage({
+    super.key,
+    required this.festival,
+  });
+
+  final Festival festival;
+
+  @override
+  State<StatefulWidget> createState() => _DetailPageSate();
+}
+
+class _DetailPageSate extends State<DetailsPage> {
+  TableRow row(String property, String value) {
+    return TableRow(children: [
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: Text(property),
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: Text(value),
+      ),
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String? status;
+
+    switch (widget.festival.status) {
+      case EventStatus.COMPLETE:
+        status = "Complete";
+        break;
+      case EventStatus.LAST_TICKETS:
+        status = "Last Ticket";
+        break;
+      case EventStatus.OPEN_TICKETING:
+        status = "Open Ticketing";
+        break;
+      default:
+        status = "Unknown status";
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+      child: Column(children: [
+        Table(
+          defaultColumnWidth: const IntrinsicColumnWidth(),
+          children: [
+            row("Name : ", widget.festival.name),
+            row("Status : ", status),
+            row("Adresse : ",
+                "${widget.festival.city} (${widget.festival.postalCode})"),
+            row("Theme : ", widget.festival.majorField),
+            row("Available tickets : ",
+                widget.festival.availableTickets.toString()),
+            row("Website : ", widget.festival.webSite),
+          ],
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton.icon(
+          onPressed: () {
+            context.goNamed('createFest');
+          },
+          icon: const Icon(Icons.add),
+          label: const Text(
+            "Create",
+          ),
+        ),
+      ]),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:festival/details.dart';
 import 'package:festival/firebase_options.dart';
 import 'package:festival/login.dart';
+import 'package:festival/models/festival.dart';
 import 'package:festival/register.dart';
 import 'package:festival/services/auth.service.dart';
 import 'package:festival/services/user.service.dart';
@@ -15,22 +17,26 @@ void main() async {
   runApp(const MyApp());
 }
 
-final GoRouter _router = GoRouter(
-  errorBuilder: errorPageBuilder,
-  redirect: (context, state) {
-    final isLoggedIn = UserService().isLoggedIn();
-    final isLoggingIn = state.location == '/login';
-    final isRegistering = state.location == '/register';
+Festival? festival;
 
-    if (!isLoggedIn) {
-      return (!isRegistering && !isLoggedIn) ? '/login' : null;
-    } else if (isLoggingIn) {
-      return '/home';
-    }
-    return null;
+final GoRouter _router = GoRouter(
+  errorBuilder: (context, state) => ErrorPage(state: state),
+  redirect: (context, state) {
+    debugPrint(state.location.toString());
+    //   final isLoggedIn = UserService().isLoggedIn();
+    //   final isLoggingIn = state.location == '/login';
+    //   final isRegistering = state.location == '/register';
+
+    //   if (!isLoggedIn) {
+    //     if (!isRegistering && !isLoggedIn) return '/login';
+    //   } else if (isLoggingIn) {
+    //     return '/home';
+    //   }
+
+    //   return null;
   },
   refreshListenable: UserService(),
-  routes: <GoRoute>[
+  routes: [
     GoRoute(
       name: 'login',
       path: '/login',
@@ -42,13 +48,60 @@ final GoRouter _router = GoRouter(
       builder: (context, state) => const RegisterPage(),
     ),
     GoRoute(
-      name: 'home',
-      path: '/home',
-      builder: (context, state) => const HomePage(),
-    )
+      name: 'error',
+      path: '/error',
+      builder: (context, state) => ErrorPage(state: state),
+    ),
+    ShellRoute(
+        builder: ((context, state, child) => ScaffoldWithBottomNavBar(
+              tabs: const [
+                ScaffoldWithNavBarTabItem(
+                  initialLocation: '/home',
+                  icon: Icon(Icons.home),
+                  label: 'Home',
+                ),
+                ScaffoldWithNavBarTabItem(
+                  initialLocation: '/details/1',
+                  icon: Icon(Icons.settings),
+                  label: 'Details',
+                ),
+              ],
+              child: child,
+            )),
+        routes: [
+          GoRoute(
+            name: 'home',
+            path: '/home',
+            pageBuilder: (context, state) => const NoTransitionPage(
+              child: HomePage(),
+            ),
+          ),
+          GoRoute(
+            redirect: (context, state) async {
+              debugPrint(state.location.toString());
+              String? uid = state.params['uid'];
+              if (uid != null) {
+                festival = await getFestival(uid);
+                if (festival != null) {
+                  return null;
+                }
+              }
+
+              return "/error?errormsg=Festival%20not%20found"; // Exception('Festival not found');
+            },
+            name: 'details',
+            path: '/details/:uid',
+            pageBuilder: (context, state) =>
+                NoTransitionPage(child: DetailsPage(festival: festival!)),
+          ),
+        ])
   ],
-  initialLocation: '/home',
+  initialLocation: '/details/FEST_01053_980',
 );
+
+Future<Festival?> getFestival(String uid) {
+  return Future.value(f);
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -66,43 +119,106 @@ class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  State<StatefulWidget> createState() => _AppState();
+  State<StatefulWidget> createState() => _HomeState();
 }
 
-class _AppState extends State<HomePage> {
+class _HomeState extends State<HomePage> {
   String status = UserService().isLoggedIn() ? "Logged" : "Not logged";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Festoche'),
-        ),
-        body: Column(
-          children: [
-            const Text('APP'),
-            Text(status),
-            ElevatedButton(
-                onPressed: () {
-                  AuthService().signOut();
-                },
-                child: const Text('Sign out')),
-            ElevatedButton(
-                onPressed: () {
-                  context.goNamed('login');
-                },
-                child: const Text('Login'))
-          ],
-        ));
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('APP'),
+        Text(status),
+        ElevatedButton(
+            onPressed: () {
+              AuthService().signOut();
+            },
+            child: const Text('Sign out')),
+        ElevatedButton(
+            onPressed: () {
+              context.goNamed('login');
+            },
+            child: const Text('Login'))
+      ],
+    );
   }
 }
 
-Widget errorPageBuilder(BuildContext context, GoRouterState state) {
-  return MaterialPage(
+class ErrorPage extends StatelessWidget {
+  const ErrorPage({super.key, required this.state});
+
+  final GoRouterState state;
+
+  @override
+  Widget build(BuildContext context) {
+    String errorMsg = (state.error != null)
+        ? state.error.toString()
+        : state.queryParams['errormsg'].toString();
+    return Scaffold(
       key: state.pageKey,
-      child: Scaffold(
-        body: Center(
-          child: Text(state.error.toString()),
-        ),
-      )) as Widget;
+      body: Center(
+        child: Text(errorMsg),
+      ),
+    );
+  }
+}
+
+class ScaffoldWithBottomNavBar extends StatefulWidget {
+  const ScaffoldWithBottomNavBar(
+      {Key? key, required this.child, required this.tabs})
+      : super(key: key);
+  final Widget child;
+  final List<ScaffoldWithNavBarTabItem> tabs;
+
+  @override
+  State<ScaffoldWithBottomNavBar> createState() =>
+      _ScaffoldWithBottomNavBarState();
+}
+
+class _ScaffoldWithBottomNavBarState extends State<ScaffoldWithBottomNavBar> {
+  // getter that computes the current index from the current location,
+  // using the helper method below
+  int get _currentIndex => _locationToTabIndex(GoRouter.of(context).location);
+
+  int _locationToTabIndex(String location) {
+    final index =
+        widget.tabs.indexWhere((t) => location.startsWith(t.initialLocation));
+    // if index not found (-1), return 0
+    return index < 0 ? 0 : index;
+  }
+
+  // callback used to navigate to the desired tab
+  void _onItemTapped(BuildContext context, int tabIndex) {
+    if (tabIndex != _currentIndex) {
+      // go to the initial location of the selected tab (by index)
+      context.go(widget.tabs[tabIndex].initialLocation);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Festoche'),
+      ),
+      body: widget.child,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: widget.tabs,
+        onTap: (index) => _onItemTapped(context, index),
+      ),
+    );
+  }
+}
+
+class ScaffoldWithNavBarTabItem extends BottomNavigationBarItem {
+  const ScaffoldWithNavBarTabItem(
+      {required this.initialLocation, required Widget icon, String? label})
+      : super(icon: icon, label: label);
+
+  /// The initial location/path
+  final String initialLocation;
 }
