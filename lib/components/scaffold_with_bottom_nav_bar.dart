f7c0fbbaad17047dddf3f15044f6085a4554c9d0
Florian <florian.desmortreux@etudiant.univ-rennes.fr>
2022-11-24 10:21:52 +0100

    navBar widget in separate file

---
+++ lib/components/scaffold_with_bottom_nav_bar.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class ScaffoldWithBottomNavBar extends StatefulWidget {
+  const ScaffoldWithBottomNavBar(
+      {Key? key, required this.child, required this.tabs})
+      : super(key: key);
+  final Widget child;
+  final List<ScaffoldWithNavBarTabItem> tabs;
+
+  @override
+  State<ScaffoldWithBottomNavBar> createState() =>
+      _ScaffoldWithBottomNavBarState();
+}
+
+class _ScaffoldWithBottomNavBarState extends State<ScaffoldWithBottomNavBar> {
+  // getter that computes the current index from the current location,
+  // using the helper method below
+  int get _currentIndex => _locationToTabIndex(GoRouter.of(context).location);
+
+  int _locationToTabIndex(String location) {
+    final index =
+        widget.tabs.indexWhere((t) => location.startsWith(t.initialLocation));
+    // if index not found (-1), return 0
+    return index < 0 ? 0 : index;
+  }
+
+  // callback used to navigate to the desired tab
+  void _onItemTapped(BuildContext context, int tabIndex) {
+    if (tabIndex != _currentIndex) {
+      // go to the initial location of the selected tab (by index)
+      context.go(widget.tabs[tabIndex].initialLocation);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Festoche'),
+      ),
+      body: widget.child,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: widget.tabs,
+        onTap: (index) => _onItemTapped(context, index),
+      ),
+    );
+  }
+}
+
+class ScaffoldWithNavBarTabItem extends BottomNavigationBarItem {
+  const ScaffoldWithNavBarTabItem(
+      {required this.initialLocation, required Widget icon, String? label})
+      : super(icon: icon, label: label);
+
+  /// The initial location/path
+  final String initialLocation;
+}
